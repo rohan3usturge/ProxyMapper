912565b798d6f5db521ec5a87d1bfd1e4a9408e7
Rohan Usturge <rohan.cesc@gmail.com>
2017-07-04 13:58:10 +0530

    Removing .netstandard support

---
+++ ProxyMapper/Enums/DatatableAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProxyMapper.Enums
+{
+    public class DatatableAttribute : Attribute
+    {
+        public DatatableAttribute(string columns)
+        {
+            this.Columns = columns;
+        }
+
+        public string Columns { get; }
+    }
+}
